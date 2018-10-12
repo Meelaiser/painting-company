403d3b22883af9e63f574c1b2566123e38f20fce
Meelaiser <44062853+Meelaiser@users.noreply.github.com>
2018-10-13 00:09:40 +0800

    Update painting.cpp

--- painting.cpp
+++ painting.cpp
@@ -1,32 +1,36 @@
-#include <iostram>
+#include <iostream>
 // Use the ceil function
-#include <cmath>
+//#include <cmath>
 
 using namespace std;
 
 // function headers
 int getNumOfRoom();
-double getTotalSpace(int num);
+int getTotalSpace(int);
+char choosePaint();
+int getPaintCost(char, int);
+int getLabourCharges(int);
+void breakdown(int, int);
 
 int main() {
 	// start with some introduction messages
-	cout << "Welcome to Saqib's painting company.";	
+	cout << "Welcome to Saqib's painting company.\n";	
 
 	// the number of room
-	int	numOfroom = getNumOfRoom();
+	int numOfroom = getNumOfRoom();
 
 	// the total space
-	double totalSpace =	getTotalSpace(numOfroom);
+	int totalSpace = getTotalSpace(numOfroom);
 
-	// displays the following choices of paint for user to choose
-	cout << "Brand" << \t << "Cost per 5 litres" << endl
-		 << "A    " << \t << "RM75" << endl
-		 << "B    " << \t << "RM100" << endl
-		 << "C    " << \t << "RM120" << endl
-	cout << endl << "The band you choose is: ";
-	cin >> band;
+	//char band;
+	char band = choosePaint();
+
+	int painCost = getPaintCost(band, totalSpace);
+	int labourCharge = getLabourCharges(totalSpace);
 
 	// display 
+	breakdown(painCost, labourCharge);
+
 	// The number of litre of paint required
 	return 0;
 }
@@ -40,44 +44,78 @@ int getNumOfRoom() {
 	int num;
 // prompts the user to enter the number of rooms
 	cout << "Enter the number of the rooms: ";
-	cin num;
+	cin >> num;
 	return num;
 }
 
 // get the wall space of each room and calculate the total wall space in square metre
-double getTotalSpace(int num) {
-	double totalSpace;
+int getTotalSpace(int num) {
+	int total = 0;
 
 	// prompts the user to enter the square metre of wall space
 	for (int a = 1; a <= num; a++) {
 		cout << "enter the square metre of wall space for room#" << a << ": ";
+		int square;
 		cin >> square;
-		totalSpace += square;
+		total += square;
 	}
 
-	return totalSpace;
+	return total;
 }
 
 // calculate the cost of the paint
-int getCost(int price, litre) {
+int getPaintCost(char band, int square) {
+	int price;
+	switch (band) {
+		case 'A':
+			price = 75; 	
+			break;
+		case 'B':
+		   	price = 100;	
+			break;
+		case 'C': 
+			price = 120;	
+			break;
+	}
+	
 	int amount;
-	if (litre % 5 > 0)
-		amount = litre / 5 + 1;
+	if (square % 5 > 0)
+		amount = square / 5 + 1;
 	else 
-		amount = litre / 5;
-
-	return priceOf5 * amount;
+		amount = square / 5;
+	
+	return price * amount;
 }
 
 // calculate the labour hours and labour charges
-int getCharges(int hour) {
-	return hour * 60;	
+int getLabourCharges(int square) {
+	int charge = 60;
+	int hour;
+
+	if (square % 3 > 0)
+		hour = square / 3 + 1;
+	else 
+		hour = square / 3;
+
+	return charge * hour;
 }
 
 // display the breakdown of the cost and the total cost
-void breakdown(int cost, int hour, int labor) {	
-	 cout << "The cost of the paint is " << cost << endl
-	 cout << "The hours of labor required is " << hour << endl
-	 cout << "The labor charges is " << labor << endl
-	 cout << "The total cost of the paint job is " << labor <<< endl
+void breakdown(int paint, int labour) {	
+	 cout << "The cost of the paint is " << paint + labour << endl
+	      << "The hours of labor required is " << labour / 60 << endl
+	      << "The labor charges is " << labour << endl
+	      << "The total cost of the paint job is " << paint << endl;
+}
+
+char choosePaint() {
+	// displays the following choices of paint for user to choose
+	cout << "Brand" << "\t" << "Cost per 5 litres" << endl
+		 << "A    " << "\t" << "RM75" << endl
+		 << "B    " << "\t" << "RM100" << endl
+		 << "C    " << "\t" << "RM120" << endl;
+	cout << endl << "The band you choose is: ";
+	char band;
+	cin >> band;
+	return band;
 }
